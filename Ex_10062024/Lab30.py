a30011070466d021e22a6fa9dace714033daa5ee
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2024-06-10 08:15:10 +0530

    Class 4

---
+++ Ex_10062024/Lab30.py
@@ -0,0 +1,14 @@
+
+# Ternary Operator
+sangram_marks = 98
+amit_marks = 97
+
+#   x > y -> do something - print("pramod")
+# y > x -> do something else -> print("amit)
+print("sangram is winner" if sangram_marks > amit_marks else "amit is winner")
+
+
+if sangram_marks > amit_marks:
+    print("sangram is the winner")
+else:
+    print("Amit is the winner")

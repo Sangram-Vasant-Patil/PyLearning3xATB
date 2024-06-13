4788ad4c2d9bbf9b9823e9d7d55cee88953e6f03
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2024-06-13 21:32:58 +0530

    12 june

---
+++ Ex_12062024/Lab43.py
@@ -0,0 +1,10 @@
+age = 30
+name = "Sangram"
+
+if name == "Sangram":
+    if age == 30:
+        print("Allowed")
+    else:
+        print("Not Allowed!, wrong age")
+else:
+    print("Wrong Name")

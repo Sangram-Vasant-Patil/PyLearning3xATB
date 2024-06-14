1e09330cdb4748838ef28d5a83dd28a2821cf76d
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2024-06-14 07:10:56 +0530

    Assignment 1

---
+++ ASSIGNMENTS-01/AssignmentTask01.py
@@ -0,0 +1,8 @@
+#Explain the difference between the = operator and the == operator in Python.
+
+#= is for assignment, while == is for comparison
+
+x = 5
+y = 12
+
+print(x == y)
---
+++ ASSIGNMENTS-01/AssignmentTask02.py
@@ -0,0 +1,7 @@
+#Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+
+number = int(input("Enter a number:"))
+Square = number ** 2
+Cube = number ** 3
+
+print(f"For the number {number}, square is {Square} and cube is {Cube}.")

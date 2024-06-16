b4d4fd6e891b37f9156545c351beadf5373461cb
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2024-06-16 22:55:53 +0530

    14th June Lab uploads

---
+++ Ex_14062024/Lab51.py
@@ -0,0 +1,20 @@
+name = str(input("Enter your name: "))
+
+
+match name:
+
+        case "DELL":
+
+            print("Dell is allowed")
+
+        case "Shubham":
+
+            print("Shubham is allowed")
+
+        case "Pallavi":
+
+            print("Pallavi is allowed")
+
+        case _:
+
+            print("Not Allowed")
